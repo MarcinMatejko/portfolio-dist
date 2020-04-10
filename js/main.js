ff5c7b1965bd5e32018d2ea02927be1e570accd2
Marcin Matejko <matejko.tsp@gmail.com>
2020-04-10 12:17:07 +0200

    dodanie js do about me zwiazany z display

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ navbar = document.getElementById('navbar');
 
 let lastScrollTop = 0;
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > lastScrollTop) {
@@ -11,4 +11,28 @@ window.addEventListener('scroll', function() {
     navbar.style.top = '0';
   }
   lastScrollTop = scrollTop;
+});
+
+const showBtn = document.querySelector('#show-btn');
+const hideBtn = document.querySelector('#hide-btn');
+const about = document.querySelector('#about');
+showBtn.addEventListener('click', function () {
+  if (about.style.display == 'block') {
+    about.style.display = 'none';
+    showBtn.innerText = 'Więcej o mnie';
+  } else {
+    about.style.display = 'block';
+    showBtn.innerText = 'Zwiń o mnie';
+    document.getElementById('about').scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
+
+hideBtn.addEventListener('click', function () {
+  about.style.display = 'none';
+  showBtn.innerText = 'Więcej o mnie';
+  document.getElementById('home').scrollIntoView({
+    behavior: 'smooth',
+  });
 });
